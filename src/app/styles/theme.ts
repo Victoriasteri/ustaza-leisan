506571544ffbf5f3b8f0bf1c30aaa80cdc99014b
Victoria Bezazieva <maryamhanifa90@gmail.com>
2024-10-20 15:44:15 +0300

    initial commit

---
+++ src/app/styles/theme.ts
@@ -0,0 +1,19 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#789dbc",
+      light: "#a1bad0",
+      dark: "#485e71",
+    },
+    secondary: {
+      main: "#c9e9d2",
+      light: "#d4eddb",
+      dark: "#a1baa8",
+    },
+    // Add more color customizations as needed
+  },
+});
+
+export default theme;
