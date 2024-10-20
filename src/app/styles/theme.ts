3ed2e88041f3d6a9587ec6a8ef2152593abc132c
Victoria Bezazieva <maryamhanifa90@gmail.com>
2024-10-20 19:33:40 +0300

    feat(): added navbar

--- src/app/styles/theme.ts
+++ src/app/styles/theme.ts
@@ -6,11 +6,13 @@ const theme = createTheme({
       main: "#789dbc",
       light: "#a1bad0",
       dark: "#485e71",
+      contrastText: "#fff",
     },
     secondary: {
       main: "#c9e9d2",
       light: "#d4eddb",
       dark: "#a1baa8",
+      contrastText: "#000",
     },
     // Add more color customizations as needed
   },
